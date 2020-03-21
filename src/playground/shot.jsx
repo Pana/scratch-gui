9a0436548a39f1ae44345e6aacf5d1a5bcd9c891
Pana Wang <pana.wang@outlook.com>
2020-03-21 18:21:24 +0800

    init project

---
+++ src/playground/shot.jsx
@@ -0,0 +1,82 @@
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {connect} from 'react-redux';
+import {compose} from 'redux';
+import axios from 'axios';
+import html2canvas from 'html2canvas';
+
+import Box from '../components/box/box.jsx';
+import GUI from '../containers/gui.jsx';
+import HashParserHOC from '../lib/hash-parser-hoc.jsx';
+import AppStateHOC from '../lib/app-state-hoc.jsx';
+
+import {setPlayer} from '../reducers/mode';
+
+if (process.env.NODE_ENV === 'production' && typeof window === 'object') {
+    // Warn before navigating away
+    window.onbeforeunload = () => true;
+}
+
+import styles from './player.css';
+
+const MCPROJECTHOST = 'http://localhost:3000';
+
+const generateShot = function () {
+    const hashMatch = window.location.hash.match(/#(\d+)/);
+    const hashProjectId = hashMatch === null ? 0 : hashMatch[1];
+    html2canvas(document.querySelector("div[class^=stage-wrapper_stage-canvas-wrapper]")).then(canvas => {
+        axios.post(`${MCPROJECTHOST}/setProjectShot`, {
+            data: canvas.toDataURL(),
+            id: hashProjectId
+        }).then(result => {
+            alert(result.statusText);
+        });
+    });
+};
+
+const Player = ({isPlayerOnly, onSeeInside, projectId}) => (
+    <Box className={classNames(isPlayerOnly ? styles.stageOnly : styles.editor)}>
+        {isPlayerOnly && <button onClick={onSeeInside}>{'See inside'}</button>}
+        <GUI
+            canEditTitle
+            enableCommunity
+            isPlayerOnly={isPlayerOnly}
+            projectId={projectId}
+        />
+        <button style={{marginTop: '10px'}} onClick={generateShot}>{'生成截图'}</button>
+    </Box>
+);
+
+Player.propTypes = {
+    isPlayerOnly: PropTypes.bool,
+    onSeeInside: PropTypes.func,
+    projectId: PropTypes.string
+};
+
+const mapStateToProps = state => ({
+    isPlayerOnly: state.scratchGui.mode.isPlayerOnly
+});
+
+const mapDispatchToProps = dispatch => ({
+    onSeeInside: () => dispatch(setPlayer(false))
+});
+
+const ConnectedPlayer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Player);
+
+// note that redux's 'compose' function is just being used as a general utility to make
+// the hierarchy of HOC constructor calls clearer here; it has nothing to do with redux's
+// ability to compose reducers.
+const WrappedPlayer = compose(
+    AppStateHOC,
+    HashParserHOC
+)(ConnectedPlayer);
+
+const appTarget = document.createElement('div');
+document.body.appendChild(appTarget);
+
+ReactDOM.render(<WrappedPlayer isPlayerOnly />, appTarget);
